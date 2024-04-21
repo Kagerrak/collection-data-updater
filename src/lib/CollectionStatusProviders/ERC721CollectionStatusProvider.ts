75208e3f037bd6d113bd467e88ce219cdf792dd5
Kage <romzhey@gmail.com>
2024-04-21 23:14:48 +0800

    Changed maxSupply to totalSupply in ERC721CollectionStatusProvider

--- src/lib/CollectionStatusProviders/ERC721CollectionStatusProvider.ts
+++ src/lib/CollectionStatusProviders/ERC721CollectionStatusProvider.ts
@@ -6,32 +6,39 @@ import { isNewMint } from "../Runtimes/UpdateTokenOnMintRuntime";
 
 export const EVENT_DATA_IS_REVEALED = "__isRevealed";
 
-export const isRevealed = (eventData: EventDataInterface): boolean|undefined => {
+export const isRevealed = (
+  eventData: EventDataInterface
+): boolean | undefined => {
   return eventData[EVENT_DATA_IS_REVEALED];
 };
 
-export const EVENT_DATA_FORCED_COLLECTION_STATUS_REFRESH = "__forcedCollectionStatusRefresh";
+export const EVENT_DATA_FORCED_COLLECTION_STATUS_REFRESH =
+  "__forcedCollectionStatusRefresh";
 
-export const forcedCollectionStatusRefresh = (eventData: EventDataInterface): boolean|undefined => {
+export const forcedCollectionStatusRefresh = (
+  eventData: EventDataInterface
+): boolean | undefined => {
   return eventData[EVENT_DATA_FORCED_COLLECTION_STATUS_REFRESH];
 };
 
-export default class ERC721CollectionStatusProvider implements CollectionStatusProviderInterface {
+export default class ERC721CollectionStatusProvider
+  implements CollectionStatusProviderInterface
+{
   private totalSupply: BigNumber = BigNumber.from(0);
   private tokenIds: BigNumber[] = [];
   private readonly startTokenId: BigNumber;
 
   public constructor(
     private contract: Contract,
-    startTokenId: BigNumber|number = 1,
+    startTokenId: BigNumber | number = 1
   ) {
     this.startTokenId = BigNumber.from(startTokenId);
   }
 
   public async getTokenIds(): Promise<BigNumber[]> {
     if (this.tokenIds.length === 0) {
-      const maxSupply = await this.contract.maxSupply();
-      
+      const maxSupply = await this.contract.totalSupply();
+
       for (let i = this.startTokenId; i.lte(maxSupply); i = i.add(1)) {
         this.tokenIds.push(i);
       }
@@ -40,19 +47,28 @@ export default class ERC721CollectionStatusProvider implements CollectionStatusP
     return this.tokenIds;
   }
 
-  public async processEventDataBeforeUpdate(eventData: EventDataInterface): Promise<EventDataInterface> {
+  public async processEventDataBeforeUpdate(
+    eventData: EventDataInterface
+  ): Promise<EventDataInterface> {
     if (isNewMint(eventData) === true) {
       if (eventData.tokenId.gt(this.totalSupply)) {
         this.totalSupply = eventData.tokenId;
       }
 
-      return { [EVENT_DATA_IS_REVEALED]: true, ...eventData }
+      return { [EVENT_DATA_IS_REVEALED]: true, ...eventData };
     }
 
-    if (forcedCollectionStatusRefresh(eventData) === true || (isFullUpdate(eventData) === true && eventData.tokenId.eq(this.startTokenId))) {
+    if (
+      forcedCollectionStatusRefresh(eventData) === true ||
+      (isFullUpdate(eventData) === true &&
+        eventData.tokenId.eq(this.startTokenId))
+    ) {
       this.totalSupply = await this.contract.totalSupply();
     }
 
-    return { [EVENT_DATA_IS_REVEALED]: eventData.tokenId.lte(this.totalSupply), ...eventData }
+    return {
+      [EVENT_DATA_IS_REVEALED]: eventData.tokenId.lte(this.totalSupply),
+      ...eventData,
+    };
   }
 }
